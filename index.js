6d17bb848e004a967919d87dffc96f3a8e8ed406
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-01 01:14:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return (" Welcome," + name + " You " + " are " + " number " + katzDeliLine.length + " in " + " line.");
+  return ("Welcome, " + name + "You " + "are " + "number " + katzDeliLine.length + " in" + " line.");
 }
 
 function nowServing(katzDeliLine) {
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
   else
-    return (" Currently " + " serving " + katzDeliLine.shift() + ".")
+    return ("Currently " + "serving " + katzDeliLine.shift() + ".")
 }
 
 function currentLine(katzDeliLine) {
