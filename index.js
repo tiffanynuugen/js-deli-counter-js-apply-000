a9560dfa53c09194850e6b04037b66758018bb4b
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-01 01:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
   else
-    return ("Currently " + "serving" + katzDeliLine.shift() + ".")
+    return ("Currently " + "serving " + katzDeliLine.shift() + ".")
 }
 
 function currentLine(katzDeliLine) {
