9a6daa3f36ac20c284cc361b005d794c8a2edf78
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-01 03:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ function currentLine(katzDeliLine) {
   for (let i = 0; i < katzDeliLine.length; i++) {
   message += (i+1) + ". " + katzDeliLine[i] + ", "
   }
-  return message.slice(0, message.length - 2);
-  }
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
 }
+else
+  return message.slice(0, message.length - 2);
+}
+
