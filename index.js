e5dfbded1dc0af5e58a22814338964ecef7c9f87
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-01 00:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function takeANumber(currentLine, name) {
 function nowServing(currentLine) {
   while (i < currentLine.length) {
     i++;
-  }
+  }   
   if (currentLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-}
+  
+  
+
